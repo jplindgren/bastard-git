5965b7be1e953b1dba29ae9ca22e1acb4ea873ce
João Paulo Pozo Lindgren <joaopozo@gmail.com>
2024-06-24 17:12:47 -0300

    chore: update goreleaser action (#11)

--- cmd/version.go
+++ cmd/version.go
@@ -11,7 +11,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "return the current bgit version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Fprintln(os.Stdout, "0.0.5")
+		fmt.Fprintln(os.Stdout, "0.0.6")
 	},
 }
 
